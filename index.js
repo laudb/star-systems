616dc62c595a89bdf4b313448f91a9d369cfa427
Laud Bentil <laudbentil@gmail.com>
2020-08-06 20:09:18 +0000

    update base files

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => { res.send('solar systems present'); });
-
-app.listen( PORT, () => { console.log('0 solar systems at', PORT) });
