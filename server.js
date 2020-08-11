c85858f844eeba69c0f89f8578dfb744a760d687
Laud Bentil <laudbentil@gmail.com>
2020-08-11 01:34:39 +0000

    add pug template engine

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const logger = require('morgan');
 const PORT = process.env.PORT || 3000;
 
 const routes = require('./api/routes');
+
 app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
+
 routes(app);
 
 app.listen(PORT, () => { console.log('0 solar systems at', PORT) });
