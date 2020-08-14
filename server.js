9046f9119a6aad04e1a2a8824c6bb0cf37ee8ed9
Laud Bentil <laudbentil@gmail.com>
2020-08-14 15:40:56 +0000

    add swagger-ui, and initial specs for endpoints

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 // app
 var express = require('express');
 const swaggerJSDoc = require('swagger-jsdoc');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+const logger = require('morgan');
+
+const PORT = process.env.PORT || 3000;
 const app = express();
+
 const swaggerDefinition = {
     info: {
         title: "Swagger API",
         version: "1.0.0",
         description: "RESTful API"
     },
-    host: "localhost: 3000",
+    host: "localhost:3000",
     basePath: "/",
     swagger: "2.0",
     paths: {},
-    definitions: {},
+    definitions: {
+
+    },
     responses: {},
     parameters: {},
     securityDefinitions: {}
@@ -20,17 +29,12 @@ const swaggerDefinition = {
 
 const options = {
     swaggerDefinition: swaggerDefinition,
-    apis: ['./routes/*.js'],
+    apis: ['./*.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const path = require('path');
-const logger = require('morgan');
 
 // server 
-const PORT = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,11 +49,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+app.use('/api-docs', express.static(__dirname + '/api-docs'));
+
 // data store
 let systems = [];
 
 // routes
+
+// --swagger 
+app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
 // --app
+/**
+ * @swagger
+ * definitions:
+ *   System:
+ *     name:
+ *       type: string
+ * /system:
+ *  post:
+ *     tags:
+ *       - Systems
+ *     description: Adds a new system
+ *     parameters:
+ *       - in: body
+ *         name: system-name
+ *         schema:
+ *           $ref: '#/definitions/System'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       201:
+ *         description: System created
+ *         schema:
+ *           $ref: '#/definitions/System'
+ *
+*/
 app.post('/system', (req, res) => {
     let name = req.body['system-name'];
     if (systems.includes(name)) {
@@ -63,24 +101,63 @@ app.post('/system', (req, res) => {
     }
 });
 
-// numbed out /add route for now with modal active
-// app.get('/add', (req, res) => {
-//     res.render('add', { data: 'Planetary Systems', title: 'Add' })
-// });
-
+/**
+ * @swagger
+ * definitions:
+ *   System:
+ *     name:
+ *       type: object
+ * /system/{name}:
+ *   get:
+ *     tags:
+ *       - SystemDetail   
+ *     description: Returns a System by the name
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *     produces: 
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: System details
+ *         schema: 
+ *           $ref: '#/definitions/System'
+ * 
+*/
 app.get('/system/:name', (req, res) => {
     res.render('details', { data: 'Planetary Systems', title: 'Details' });
 });
 
+
+/**
+ * @swagger
+ * definitions:
+ *   Systems:
+ *     properties:
+ *       name:
+ *         type: array
+ * /:
+ *   get:
+ *     tags:
+ *       - Systems
+ *     description: Returns all Systems by names
+ *     produces: 
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: List of System names
+ *         schema: 
+ *           $ref: '#/definitions/Systems'
+ * 
+*/
 app.get('/', (req, res) => {
     res.render('list', { data: 'Planetary Systems', title: 'Home', systems: systems });
 });
 
-// --swagger 
-app.get('/swagger.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-});
+// numbed out /add route for now with modal active
+// app.get('/add', (req, res) => {
+//     res.render('add', { data: 'Planetary Systems', title: 'Add' })
+// });
 
 // server
 app.listen(PORT, () => {
