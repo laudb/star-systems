77a80f1373befe522b731cc77e369ebc9de651b6
Laud Bentil <laudbentil@gmail.com>
2020-08-15 03:35:14 +0000

    update loaders with bodyParser & cookieParser setup

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // app
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
@@ -10,12 +8,6 @@ app.use(express.json());
 
 require('./loaders')(app, express);
 require('./swagger-app')(app, express);
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(cookieParser());
 
 // routes
 app.use(routes);
--- loaders/index.js
+++ loaders/index.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 // loaders
@@ -8,9 +10,17 @@ module.exports = function (app, express) {
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'pug');
 
+    // static folder setup
     app.use(express.urlencoded({ extended: false }));
     app.use(express.static(path.join(__dirname, '../public')));
 
+    // morgan
     app.use(logger('dev'));
+
+    // bodyParser & cookieParser
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+
 }
 
