583cd892494ecfa2ebc5447bab6400604b31e4e9
Laud Bentil <laudbentil@gmail.com>
2020-12-12 01:42:35 +0000

    update env variables util

--- util.js
+++ util.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = config = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  mongo: process.env.DB,
+  wolfid: process.env.WOLFID
 };
