417a67dd38ae0b91e2a6833ebe7c245dbc7468f4
Laud Bentil <laudbentil@gmail.com>
2020-12-16 23:25:12 +0000

    use one parameter for now, load image when present

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,16 @@ console.log('scripts loadeeed!');
 // fetch row data 
 $(document).ready(function () {
     $("tr").dblclick(function(e) {
+
         let rowItems = $(this).children('td').map(function() {
             return this.innerHTML;
         }).toArray();
-        
+
         let name = rowItems[1];
         let route = 'details'
         let baseUrl = window.location;
         let url = baseUrl + route;  
-
+        
         $.ajax({
             type:'POST',
             url: url,
@@ -20,7 +21,7 @@ $(document).ready(function () {
             console.log('success')
             console.log({'response>>>>': response})
             $("#wolfram-image").attr("src", response.img);
-            // $("#content-text").html(response.plaintext);
+            // $("#content-source").html(response.datasources);
         }).catch((err)=>{
             console.log({'err': err})
         })
