f6f53e86beed1a2d2d157f69c9c74b23af3db3ef
Laud Bentil <laudbentil@gmail.com>
2020-12-21 11:25:09 +0000

    update sample data

--- models/sample.js
+++ models/sample.js
@@ -1,25 +1,32 @@
 let systems = [
   {
-      id: 0,
-      name: 'Sol',
-      properName: 'Sun',
-      distance: 0.00,
-      planets: 8
+    id: 0,
+    name: "Sol",
+    properName: "Sun",
+    distance: 0.00,
+    planets: 8
   },
   {
     id: 1,
-    name: 'Sol',
-    properName: 'Sun',
-    distance: 0.00,
-    planets: 0
+    name: "Gl 551",
+    properName: "Proxima Centauri",
+    distance: 4.22,
+    planets: ''
   },
   {
-      id: 3,
-      name: 'Gl 144',
-      properName: 'Epsilon Eridani',
-      distance: 10.49,
-      planets: 1
+    id: 2,
+    name: "Gl 699",
+    properName: "Barnard's Star",
+    distance: 5.94,
+    planets: ''
   },
-]
+  {
+    id: 3,
+    name: "Gl 406",
+    properName: "Wolf 359",
+    distance: 7.79,
+    planets: ''
+  }
+]                               
 
 export default systems;
