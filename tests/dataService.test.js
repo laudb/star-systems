cc71a897bd749ce0c9bda6ee7b3a965c4067a0d5
Laud Bentil <laudbentil@gmail.com>
2021-01-04 16:25:30 +0000

    update test for getSystems

--- tests/dataService.test.js
+++ tests/dataService.test.js
@@ -15,10 +15,15 @@ const sample = testSample;
 const name = testSample[0]['properName'];
 const undefinedName = undefined;
 
-test('expect name to give "Alpha Stellaris" when using getSystem ', async () => {
+test('expect [testName] to give "Alpha Stellaris" when using getSystem ', async () => {
   let testName = testSample[0]['properName'];
   let name = await ds.getSystem(testName);
-  console.log({ 'name': [testName, name] });
   expect(name).toBe(testName);
 });
 
+test('expect [testName] to not give Alpha "Stellaris" when using getSystem', async () => {
+  let testName = 'undefined';
+  let name = await ds.getSystem(testName);
+  console.log({ 'name': [testName, name] });
+  expect(name).toEqual(expect.not.stringContaining(testName));
+})
